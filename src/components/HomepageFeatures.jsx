89a4374ea1c0d4b01b7b6c83123471b895afc136
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-04-07 12:58:10 -0500

    update home new

--- src/components/HomepageFeatures.jsx
+++ src/components/HomepageFeatures.jsx
@@ -23,6 +23,7 @@ const FeatureList = [
     ),
     Url: 'https://sveltekit-cv.vercel.app/'
   },
+  
 
 ];
 
@@ -32,7 +33,7 @@ function Feature({Svg, title, description, Url}) {
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className={styles.desc}>
         <h3>
           <a href={Url}>{title}</a>
         </h3>
